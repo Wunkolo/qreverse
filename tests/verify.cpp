f4e4ce781be8b5a9c7b4cf63104beafa337be2da
Wunkolo <wunkolo@gmail.com>
2017-08-23 02:03:34 -0700

    Update verify.cpp

--- tests/verify.cpp
+++ tests/verify.cpp
@@ -4,34 +4,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 #include <qreverse.hpp>
 
 /*
-	For use with cmake:
-		Verifies that qreverse can properly reverse an array at the designated
-		compile-time element size.
-*/
+For use with cmake:
+	Verifies that qreverse can properly reverse an array at the designated
+	compile-time element size.
 
-// Verifies that the array is indeed properly reversed
+	Define ELEMENTSIZE preprocessor value to adjust verified element size
+*/
 
 #ifndef ELEMENTSIZE
 #define ELEMENTSIZE 1
 #endif
 
-int main( int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 	if( argc < 2 )
 	{
-		std::cout << "Usage: Verify(ElementSize) (Element Count)"
+		std::cout << "Usage: Verify# (Element Count)"
 			<< std::endl;
 		return EXIT_FAILURE;
 	}
 
 	std::size_t ElementCount;
 
-	ElementCount = std::strtoull(argv[1],nullptr,10);
+	ElementCount = std::strtoull(argv[1], nullptr, 10);
 
 	if( ElementCount == 0 || ElementCount == ULLONG_MAX )
 	{
@@ -41,10 +40,10 @@ int main( int argc, char* argv[])
 	std::vector<std::uint8_t> Array(ELEMENTSIZE * ElementCount);
 
 	std::cout << "Original: " << std::endl;
-	for( std::size_t i = 0; i < ElementCount; ++i)
+	for( std::size_t i = 0; i < ElementCount; ++i )
 	{
 		std::uint8_t* Element = &Array[i * ELEMENTSIZE];
-		for( std::size_t j = 0; j < ELEMENTSIZE; ++j)
+		for( std::size_t j = 0; j < ELEMENTSIZE; ++j )
 		{
 			Element[j] = static_cast<std::uint8_t>(i);
 			std::cout << +Element[j] << ' ';
@@ -55,13 +54,13 @@ int main( int argc, char* argv[])
 
 	std::vector<std::uint8_t> Reversed(Array);
 
-	qReverse<ELEMENTSIZE>(Reversed.data(),ElementCount);
+	qReverse<ELEMENTSIZE>(Reversed.data(), ElementCount);
 
 	std::cout << "Reversed: " << std::endl;
-	for( std::size_t i = 0; i < ElementCount; ++i)
+	for( std::size_t i = 0; i < ElementCount; ++i )
 	{
 		std::uint8_t* Element = &Reversed[i * ELEMENTSIZE];
-		for( std::size_t j = 0; j < ELEMENTSIZE; ++j)
+		for( std::size_t j = 0; j < ELEMENTSIZE; ++j )
 		{
 			std::cout << +Element[j] << ' ';
 		}
@@ -70,22 +69,22 @@ int main( int argc, char* argv[])
 	std::cout << std::endl;
 
 	// Verify proper reversal
-	for( std::size_t i = 0; i < ElementCount; ++i)
+	for( std::size_t i = 0; i < ElementCount; ++i )
 	{
 		const std::uint8_t* OriginalElem = &Array[(ElementCount - i - 1) * ELEMENTSIZE];
 		const std::uint8_t* ReversedElem = &Reversed[i * ELEMENTSIZE];
-		for( std::size_t j = 0; j < ELEMENTSIZE; ++j)
+		for( std::size_t j = 0; j < ELEMENTSIZE; ++j )
 		{
 			if( OriginalElem[j] != ReversedElem[j] )
 			{
 				// Mismatch, not reversed
-				std::cout << "Array Not Reversed" << std::endl;
+				std::cout << "[FAIL] Array Not Reversed" << std::endl;
 				return EXIT_FAILURE;
 			}
 		}
 	}
 
 	// Successfully reversed
-	std::cout << "Array Reversed" << std::endl;
+	std::cout << "[PASS] Array Reversed" << std::endl;
 	return EXIT_SUCCESS;
 }
