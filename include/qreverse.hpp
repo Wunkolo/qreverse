b58b17d9a9ebdda77978dc5d8ce140ae8fef3b21
Wunkolo <wunkolo@gmail.com>
2018-04-23 09:48:13 -0700

    Implement AVX2 4-byte element swapping

--- include/qreverse.hpp
+++ include/qreverse.hpp
@@ -316,7 +316,6 @@ inline void qReverse<2>(void* Array, std::size_t Count)
 			reinterpret_cast<__m256i*>(&Array16[Count - i - 16])
 		);
 
-		// Reverse the byte order of our 32-byte vectors
 		Lower = _mm256_shuffle_epi8(Lower,ShuffleRev);
 		Upper = _mm256_shuffle_epi8(Upper,ShuffleRev);
 
@@ -352,7 +351,6 @@ inline void qReverse<2>(void* Array, std::size_t Count)
 			reinterpret_cast<__m128i*>(&Array16[Count - i - 8])
 		);
 
-		// Reverse the byte order of our 16-byte vectors
 		Lower = _mm_shuffle_epi8(Lower, ShuffleRev);
 		Upper = _mm_shuffle_epi8(Upper, ShuffleRev);
 
@@ -389,6 +387,38 @@ inline void qReverse<4>(void* Array, std::size_t Count)
 	std::uint32_t* Array32 = reinterpret_cast<std::uint32_t*>(Array);
 	std::size_t i = 0;
 
+	// AVX-2
+#if defined(__AVX2__)
+	for( std::size_t j = i; j < ((Count / 2) / 8); ++j )
+	{
+		// Load 8 elements at once into one 32-byte register
+		__m256i Lower = _mm256_loadu_si256(
+			reinterpret_cast<__m256i*>(&Array32[i])
+		);
+		__m256i Upper = _mm256_loadu_si256(
+			reinterpret_cast<__m256i*>(&Array32[Count - i - 8])
+		);
+
+		Lower = _mm256_shuffle_epi32(Lower, _MM_SHUFFLE(0,1,2,3) );
+		Upper = _mm256_shuffle_epi32(Upper, _MM_SHUFFLE(0,1,2,3) );
+
+		Lower = _mm256_permute2x128_si256(Lower,Lower,1);
+		Upper = _mm256_permute2x128_si256(Upper,Upper,1);
+
+		// Place them at their swapped position
+		_mm256_storeu_si256(
+			reinterpret_cast<__m256i*>(&Array32[i]),
+			Upper
+		);
+		_mm256_storeu_si256(
+			reinterpret_cast<__m256i*>(&Array32[Count - i - 8]),
+			Lower
+		);
+
+		// 8 elements at a time
+		i += 8;
+	}
+#endif
 	// SSSE3
 #if defined(__SSSE3__)
 	for( std::size_t j = i; j < ((Count / 2) / 4); ++j )
@@ -401,7 +431,6 @@ inline void qReverse<4>(void* Array, std::size_t Count)
 			reinterpret_cast<__m128i*>(&Array32[Count - i - 4])
 		);
 
-		// Reverse the byte order of our 16-byte vectors
 		Lower = _mm_shuffle_epi32(Lower, _MM_SHUFFLE(0,1,2,3) );
 		Upper = _mm_shuffle_epi32(Upper, _MM_SHUFFLE(0,1,2,3) );
 
@@ -450,7 +479,6 @@ inline void qReverse<8>(void* Array, std::size_t Count)
 			reinterpret_cast<__m128i*>(&Array64[Count - i - 2])
 		);
 
-		// Reverse the byte order of our 16-byte vectors
 		Lower = _mm_alignr_epi8(Lower, Lower, 8);
 		Upper = _mm_alignr_epi8(Upper, Upper, 8);
 
